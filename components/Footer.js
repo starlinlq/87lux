9f0ae980934579e434c6f17fe9b919d810e3d0dc
starlinlq <67881697+starlinlq@users.noreply.github.com>
2021-02-09 00:46:37 -0600

    team & header

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,14 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+
 const Footer = () => {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer">
+      <div className="footer__content">
+        <h1 className="title">We work with any budget</h1>
+        <button className="button">Lets Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- components/MeetTeam.js
+++ components/MeetTeam.js
@@ -4,13 +4,27 @@ import Image from "next/image";
 function MeetTeam() {
   return (
     <Container className="team">
-      <div className="team__circle"></div>
-      <div className="team__circle"></div>
-      <div className="team__circle"></div>
-      <div className="team__circle"></div>
-      <div className="team__circle"></div>
-      <div className="team__circle"></div>
-      <div className="team__circle"></div>
+      <div className="team__circle">
+        <img src="/pic1.png" className="img" alt="team1" />
+      </div>
+      <div className="team__circle">
+        <img src="/pic2.png" className="img" alt="team2" />
+      </div>
+      <div className="team__circle">
+        <img src="/pic3.png" className="img" alt="team3" />
+      </div>
+      <div className="team__circle">
+        <img src="/pic4.png" className="img" alt="team4" />
+      </div>
+      <div className="team__circle">
+        <img src="/pic5.png" className="img" alt="team5" />
+      </div>
+      <div className="team__circle">
+        <img src="/pic6.png" className="img" alt="team6" />
+      </div>
+      <div className="team__circle">
+        <img src="/pic7.png" className="img" alt="team7" />
+      </div>
       <h1 className="team__title">Meet The Team</h1>
     </Container>
   );
